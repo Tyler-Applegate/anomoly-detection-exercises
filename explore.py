e9b90e873662fa586066900486f757ba5721481b
Tyler-Applegate <tyler.applegate@gmail.com>
2021-07-15 16:16:31 -0500

    updated functions

--- explore.py
+++ explore.py
@@ -2,3 +2,193 @@
 
 # Imports
 import pandas as pd
+import numpy as np
+from pydataset import data
+import matplotlib.pyplot as plt
+import seaborn as sns
+from IPython.display import display, display_html
+
+
+# This function will work on a single column
+
+def get_lower_and_upper_bounds(df, col, k=1.5):
+    '''
+    This function takes in a dataframe and returns the upper and lower bounds based on the argument of k
+    '''
+    # Find the lower and upper quartiles
+    q_25, q_75 = df[col].quantile([0.25, 0.75])
+    # Find the Inner Quartile Range
+    q_iqr = q_75 - q_25
+    # Find the Upper Bound
+    q_upper = q_75 + (k * q_iqr)
+    # Find the Lower Bound
+    q_lower = q_25 - (k * q_iqr)
+    # Identify outliers
+    outliers = df[df[col] < q_lower]
+    outliers = df[df[col] > q_upper]
+    
+    return q_lower, q_upper, outliers
+
+############## Child Functions for IQR / Outliers ###########################
+
+def get_iqr_stats(df, col, k=1.5):
+    '''
+    This function will take in a pandas Series, and run metrics to determine quantiles, IQR, and upper/lower fences.
+    '''
+
+    # Find the lower and upper quartiles
+    q_25, q_75 = df[col].quantile([0.25, 0.75])
+    # Find the Inner Quartile Range
+    q_iqr = q_75 - q_25
+    # Find the Upper Bound
+    q_upper = q_75 + (k * q_iqr)
+    # Find the Lower Bound
+    q_lower = q_25 - (k * q_iqr)
+    # Identify outliers
+    outliers_lower = df[df[col] < q_lower]
+    outliers_upper = df[df[col] > q_upper]
+    outliers_all = pd.concat([outliers_lower, outliers_upper], axis=0)
+    
+    print(f'''
+{col}
+
+K: {k}
+IQR: {q_iqr}
+Lower Fence: {q_lower}
+Upper Fence: {q_upper}
+''')
+
+    display(outliers_lower)
+
+    display(outliers_upper)
+
+    display(outliers_all)
+        
+def plot_iqr_stats(df, col, k=1.5):
+    '''
+    This function will take in a pandas Series and plot a histogram and boxplot, with whiskers set based on value of k.
+    '''
+    plt.figure(figsize=(16,4))
+    plt.subplot(1, 2, 1)
+    sns.histplot(data = df, x = col, kde=True)
+    plt.title(col)
+    plt.subplot(1, 2, 2)
+    sns.boxplot(x=df[col], data=df, whis=k)
+    plt.title(col)
+    plt.show()
+
+        
+def get_plot_iqr_stats(df, col, k=1.5):
+    '''
+    This function takes in a pandas Series, and combines the get_iqr_stats function and plot_iqr_stats to return numerical and graphical data for the column
+    '''
+    return get_iqr_stats(df, col, k=1.5), plot_iqr_stats(df, col, k=1.5)
+
+def whole_df_iqr(df, k=1.5):
+    col_list = list(df.select_dtypes(include=['int', 'float'], exclude='O'))
+    
+    for col in col_list:
+        get_plot_iqr_stats(df, col, k=1.5)
+        
+    return df.info()
+    
+    
+    
+    
+
+# This function will take in an entire dataframe, and operate on a lit of columns...
+
+def get_low_and_up_bounds_df(df, k=1.5, sort_values=False):
+    '''
+    This function takes in a pandas dataframe, list of columns, and k value, and will print out upper and lower bounds for each column.
+    It takes in a default argument of the col_list being all numeric columns, and the k value=1.5
+    '''
+    col_list=list(df.select_dtypes(include=['int', 'float'], exclude='O'))
+    
+    if sort_values == False:
+                                   
+        for col in col_list:
+
+            # Find the lower and upper quartiles
+            q_25, q_75 = df[col].quantile([0.25, 0.75])
+            # Find the Inner Quartile Range
+            q_iqr = q_75 - q_25
+            # Find the Upper Bound
+            q_upper = q_75 + (k * q_iqr)
+            # Find the Lower Bound
+            q_lower = q_25 - (k * q_iqr)
+            # Identify outliers
+            outliers_lower = df[df[col] < q_lower]
+            outliers_upper = df[df[col] > q_upper]
+            outliers_all = pd.concat([outliers_lower, outliers_upper], axis=0)
+            
+            print('')
+            print(col)
+            print(f'K: {k}')
+            print(f'Lower Fence: {q_lower}')
+            print(f'Upper Fence: {q_upper}')
+            print('')
+            print(f'Lower Outliers in {col}')
+            print('')
+            print(outliers_lower)
+            print('')
+            print(f'Upper Outliers in {col}')
+            print('')
+            print(outliers_upper)
+            print('')
+            print(f'All Outliers in {col}')
+            print('')
+            print(outliers_all)
+            plt.figure(figsize=(16,4))
+            plt.subplot(1, 2, 1)
+            sns.histplot(data = df, x = col, kde=True)
+            plt.title(col)
+            plt.subplot(1, 2, 2)
+            sns.boxplot(x=df[col], data=df, whis=k)
+            plt.title(col)
+            plt.show()
+            print('-------------------------------------------------------------------')
+            
+    else:
+        
+        for col in col_list:
+
+            # Find the lower and upper quartiles
+            q_25, q_75 = df[col].quantile([0.25, 0.75])
+            # Find the Inner Quartile Range
+            q_iqr = q_75 - q_25
+            # Find the Upper Bound
+            q_upper = q_75 + (k * q_iqr)
+            # Find the Lower Bound
+            q_lower = q_25 - (k * q_iqr)
+            # Identify outliers
+            outliers_lower = df[df[col] < q_lower].sort_values(by=col)
+            outliers_upper = df[df[col] > q_upper].sort_values(by=col)
+            outliers_all = pd.concat([outliers_lower, outliers_upper], axis=0).sort_values(by=col)
+
+            print('')
+            print(col)
+            print(f'K: {k}')
+            print(f'Lower Fence: {q_lower}')
+            print(f'Upper Fence: {q_upper}')
+            print('')
+            print(f'Lower Outliers in {col}')
+            print('')
+            print(outliers_lower)
+            print('')
+            print(f'Upper Outliers in {col}')
+            print('')
+            print(outliers_upper)
+            print('')
+            print(f'All Outliers in {col}')
+            print('')
+            print(outliers_all)
+            plt.figure(figsize=(16,4))
+            plt.subplot(1, 2, 1)
+            sns.histplot(data = df, x = col, kde=True)
+            plt.title(col)
+            plt.subplot(1, 2, 2)
+            sns.boxplot(x=df[col], data=df, whis=k)
+            plt.title(col)
+            plt.show()
+            print('-------------------------------------------------------------------')
